d0b8ba28c262518003cb4ca9a91860e756cab032
riporippo <22fi124@ms.dendai.ac.jp>
2024-05-27 16:44:07 +0900

    課題1クリア

--- socket-3/src/kadai/UDPClient.java
+++ socket-3/src/kadai/UDPClient.java
@@ -1,5 +1,37 @@
 package kadai;
-
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.util.Scanner;
 public class UDPClient {
-    
+    public static void main(String[] args){
+        DatagramSocket socket = null;
+        try{
+            socket = new DatagramSocket();
+            InetAddress serverAddress = InetAddress.getByName("localhost");
+            byte[] sendData;
+            byte[] receiveData = new byte[1024];
+            Scanner scanner = new Scanner(System.in);
+
+            while(true){
+                System.out.println("メッセージを送信しましょう(ローマ字で)");
+                String sendMessage = scanner.nextLine();
+                sendData = sendMessage.getBytes();
+
+                DatagramPacket sendPacket = new DatagramPacket(sendData,sendData.length,serverAddress,8080);
+                socket.send(sendPacket);
+
+                DatagramPacket receivePacket = new DatagramPacket(receiveData,receiveData.length);
+                socket.receive(receivePacket);
+                String serverResponse = new String(receivePacket.getData(),0,receivePacket.getLength());
+                System.out.println(serverResponse);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            if(socket!=null && !socket.isClosed()){
+                socket.close();
+            }
+        }
+    }
 }
--- socket-3/src/kadai/UDPServer.java
+++ socket-3/src/kadai/UDPServer.java
@@ -1,5 +1,37 @@
 package kadai;
-
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 public class UDPServer {
-    
+    public static void main(String[] args){
+        DatagramSocket socket = null;
+        try{
+            socket = new DatagramSocket(8080);
+            byte[] receiveData = new byte[1024];
+            byte[] sendData;
+
+            while(true){
+                DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+                System.out.println("クライアントからの入力待機");
+
+                socket.receive(receivePacket);
+                String receiveMessage = new String(receivePacket.getData(),0,receivePacket.getLength());
+                System.out.println(receiveMessage);
+                //大文字変換
+                sendData = receiveMessage.toUpperCase().getBytes();
+
+                InetAddress clientAddress = receivePacket.getAddress();
+                int clientPort = receivePacket.getPort();
+
+                DatagramPacket sendPacket = new DatagramPacket(sendData,sendData.length,clientAddress,clientPort);
+                socket.send(sendPacket);
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            if(socket != null && !socket.isClosed()){
+                socket.close();
+            }
+        }
+    }
 }
